6d4b77b3e3e89c7182a1ea92de240885981d3b9d
Sam-McCathie <sammccathie2@gmail.com>
2021-03-05 14:49:17 +1300

    Adding Routes VSC

---
+++ src/SM/Pages/ProjectBuilder1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectBuilder1() {
+  return <h1>Test2</h1>;
+}
+
+export default ProjectBuilder1;
---
+++ src/SM/Pages/ProjectsPageStudent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectsPageStudent() {
+  return <h1>Test3</h1>;
+}
+
+export default ProjectsPageStudent;
