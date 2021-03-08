e298f06a7140851f3534e3a28531b078ff6b05f9
gar212 <gartoyou@gmail.com>
2021-03-08 17:36:41 +1300

    Add SideNav collapse arrow

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SideNav.css';
 import Sidebar1 from '../img/Sidebar1.png';
 import Sidebar2 from '../img/Sidebar2.png';
@@ -7,24 +7,37 @@ import Sidebar4 from '../img/Sidebar4.png';
 import Sidebar5 from '../img/Sidebar5.png';
 import TeacherProfile from '../img/teacherProfile.jpg';
 
-const SideNav = (props) => {
-    return (
-                <div className="sideNav">
-                    <img className="profileImage" src={TeacherProfile} alt="Teacher Profile"/>
-                    <ul className="sideNavUpper">
-                        <li><img src={Sidebar1} alt="Progress Tracker Icon" />Progress Tracker</li>
-                        <li><img src={Sidebar2} alt="Student Profiles Icon" />Student Profiles</li>
-                        <li><img src={Sidebar3} alt="Help Request Icon" />Help Requests</li>
-                        <li><img src={Sidebar4} alt="Project Submission Icon" />Project Submissions</li>
-                        <li><img src={Sidebar5} alt="Project Library Icon" />Project Library</li>
-                    </ul>
+function SideNav(props){
+    // State for SideNav slide in/out function
+    const [sideNavOpened, setSideNavOpened] = useState(true); 
+    
+    // Apply style based on state of sideNavOpened
+    const slideSideNav = () => { 
+        return sideNavOpened ? {} : {width: "110px"}
+    };
+    
+    // Toggle state of sideNavOpened
+    const slideButtonClicked = () => {
+        setSideNavOpened(!sideNavOpened)
+    };
 
-                    <ul className="sideNavLower">
-                        <li>Profile</li>
-                        <li>Settings</li>
-                        <li>Log out</li>
-                    </ul>
-                </div>
+    return (
+    <div className="sideNav" style={slideSideNav()}>
+        <img className="profileImage" src={TeacherProfile} alt="Teacher Profile"/>
+        <ul className="sideNavUpper">
+            <li><img src={Sidebar1} alt="Progress Tracker Icon" />{sideNavOpened ? 'Progress Tracker' : ''}</li>
+            <li><img src={Sidebar2} alt="Student Profiles Icon" />{sideNavOpened ? 'Student Profiles' : ''}</li>
+            <li><img src={Sidebar3} alt="Help Request Icon" />{sideNavOpened ? 'Help Requests' : ''}</li>
+            <li><img src={Sidebar4} alt="Project Submission Icon" />{sideNavOpened ? 'Project Submissions' : ''}</li>
+            <li><img src={Sidebar5} alt="Project Library Icon" />{sideNavOpened ? 'Project Library' : ''}</li>
+        </ul>
+        <button className="toggleNav" onClick={()=> {slideButtonClicked()}}><i className="fa fa-caret-left"></i></button>
+        <ul className="sideNavLower">
+            <li><a href="/"><i className="fa fa-user-circle" /><label>Profile</label></a></li>
+            <li><a href="/"><i className="fa fa-cog" /><label>Settings</label></a></li>
+            <li><a href="/"><i className="fa fa-sign-out"/><label>Log out</label></a></li>
+        </ul>
+    </div>
     )
 }
 
