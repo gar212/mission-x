585fec93c67f73183b6daa0aac5d31e736c3b9a5
Sam-McCathie <sammccathie2@gmail.com>
2021-03-15 10:53:21 +1300

    fix bugs and naming error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route } from "react-router-dom";
+import {Route} from "react-router-dom";
 import TeacherDashboard from "./components/TeacherDashboard";
 import ProfileStudent from "./SM/Pages/ProfileStudent";
 import ProjectBuilder1 from "./SM/Pages/ProjectBuilder1";
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path="/teacherdashboard" component={TeacherDashboard} />
-      <Route exact path="/projectspagestudent" component={ProfileStudent} />
+      <Route exact path="/profilestudent" component={ProfileStudent} />
       <Route exact path="/projectbuilder1" component={ProjectBuilder1} />
       <Route exact path="/projectpagestudent" component={ProjectsPageStudent} />
     </div>
--- src/SM/Pages/ProfileStudent.js
+++ src/SM/Pages/ProfileStudent.js
@@ -1,7 +1,13 @@
 import React from "react";
+import TopNavBar from "../Components/TopNavBar.js";
 
 function ProfileStudent() {
-  return <h1>Test</h1>;
+  return (
+    <div>
+      <TopNavBar />
+      <h1>Test</h1>
+    </div>
+  );
 }
 
 export default ProfileStudent;
