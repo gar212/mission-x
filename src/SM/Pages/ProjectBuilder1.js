9cbe7da5eaf1c5ef46d95ff571220d90706272f2
Sam-McCathie <sammccathie2@gmail.com>
2021-04-05 16:14:38 +1200

    Layout for Project Builder + Working on sidebar

--- src/SM/Pages/ProjectBuilder1.js
+++ src/SM/Pages/ProjectBuilder1.js
@@ -1,7 +1,43 @@
 import React from "react";
+import "./ProjectBuilder1.css";
 
 function ProjectBuilder1() {
-  return <h1>Test2</h1>;
+  return (
+    <div>
+      <div className="TopNav"></div>
+      <div className="Body">
+        <div className="SideBar">
+          <div className="user">
+            <p>PHOTO - Child</p>
+          </div>
+
+          <div className="SideBarOption">
+            <p>LEARNING OBJECTIVES</p>
+          </div>
+          <div className="SideBarOption">
+            <p>INSTRUCTIONS</p>
+          </div>
+          <div className="SideBarOption">
+            <p>VIDEO TUTORIAL</p>
+          </div>
+          <div className="SideBarOption">
+            <p>MAKE PROJECT</p>
+          </div>
+          <div className="SideBarOption">
+            <p>SUBMIT PROJECT</p>
+          </div>
+          <div className="SideBarOption">
+            <p>BONUS CHALLENGE</p>
+          </div>
+          <div className="SideBarOption">
+            <p>TAKE THE QUIZ</p>
+          </div>
+        </div>
+        <div className="Content"></div>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
 }
 
 export default ProjectBuilder1;
