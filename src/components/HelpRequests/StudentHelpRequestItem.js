537f3fbd578851b890f9ee1add914a251599cd2e
RenoThompson <rthompson1708@gmail.com>
2021-04-23 18:30:21 +1200

    Edited Dashboard.js to include help request pages

--- src/components/HelpRequests/StudentHelpRequestItem.js
+++ src/components/HelpRequests/StudentHelpRequestItem.js
@@ -4,20 +4,19 @@ import './StudentHelpRequestItem.css';
 
 const StudentHelpRequestItem = (props) => {
     return (
-        // Checkbox placed in its own div so it is outside of the studenthelp request 'bubble'
         <div className="CheckboxDiv">
             <input type="Checkbox"></input>
-        <div className="StudentHelpRequestItem">
-            {/* Just a place holder image atm will be dynamic once updated */}
-            <img src={TeacherProfile} alt="Profile" />
-            
-            {/* Will update student name and timestamp once we learn more about backend */}
-            <p className="StudentHelpText">StudentName needs help with his project</p>
-            <ul className="TimeStamp">
-                <li>Date</li>
-                <li>Time</li>
-            </ul>
-        </div>
+            <div className="StudentHelpRequestItem">
+                {/* Just a place holder image atm will be dynamic once updated */}
+                <img src={TeacherProfile} alt="Profile" />
+                
+                {/* Will update student name and timestamp once we learn more about backend */}
+                <p className="StudentHelpText">StudentName needs help with his project</p>
+                <ul className="TimeStamp">
+                    <li>Date</li>
+                    <li>Time</li>
+                </ul>
+            </div>
         </div>
     )
 }
