6dc9d5c7255fe98c9e74db5a8bc8e849923fccff
gar212 <gartoyou@gmail.com>
2021-04-28 13:56:07 +1200

    add tenary op when passed null

--- src/components/ProgressTracker/ProgressStudent.js
+++ src/components/ProgressTracker/ProgressStudent.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import ProgressCircle from './ProgressCircle';
 import './ProgressStudent.css';
 
 const ProgressStudent = (props) => {
+
+    const [isLoading, setLoading] = useState(true);
+    const [apiData, setApiData] = useState([]);
+
+    useEffect(() => {
+        async function fetchAPI() {
+            let response = await fetch('/progresstracker')
+            response = await response.json()
+            setApiData(response);
+            setLoading(false);
+        }
+       fetchAPI()
+    }, [])
+
+
+
+
+
     const progressCircleItems = [];
     // Change condition based on length. Apply when we implement back-end systems 
     for (let i = 1; i <= 15; i++){
-        progressCircleItems.push(<ProgressCircle className="circleComplete" courseNumber={i} key={i}  />);
+        progressCircleItems.push(<ProgressCircle courseNumber={i} key={i}  />);
     }
 
     return (
         <div className="progressItem">
             <div className="info">
                 <p className="infoName">{props.studentName}</p>
-                <p className="infoAmount">{props.completed}/15 <span>Projects Completed</span></p>
+                <p className="infoAmount">{props.completed === null ? '0' : props.completed}/15 <span>Projects Completed</span></p>
             </div>
             <div className="circleContainer">
                 {progressCircleItems}
