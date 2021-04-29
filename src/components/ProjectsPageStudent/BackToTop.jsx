9702e8c7015bf841b60d26f26e94dc1e7b549d29
Sam-McCathie <sammccathie2@gmail.com>
2021-04-30 08:46:41 +1200

    Add js to back to top button

--- src/components/ProjectsPageStudent/BackToTop.jsx
+++ src/components/ProjectsPageStudent/BackToTop.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./BackToTop.css";
 
+function backToTop() {
+  document.documentElement.scrollTop = 0;
+}
+
 function BackToTop() {
   return (
-    <div id="BackToTop">
+    <div id="BackToTop" onClick={backToTop}>
       <h1 id="BackToTopTxt">BACK TO TOP</h1>
     </div>
   );
