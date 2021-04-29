1bc1a16f1d73d32ae13de2dab415656b83d333ec
Sam-McCathie <sammccathie2@gmail.com>
2021-04-29 13:42:27 +1200

    commit to merge

--- src/components/ProjectsPageStudent/ProjectsPageStudent.jsx
+++ src/components/ProjectsPageStudent/ProjectsPageStudent.jsx
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from "react";
 import "./ProjectsPageStudent.css";
 import TopNav from "../TopNav/TopNav.jsx";
+import ProjectsHeader from "./ProjectsHeader";
+
 import Footer from "../Footer/Footer.jsx";
 import ProjectsSidebar from "../ProjectsPageStudent/ProjectsPageSidebar.jsx";
 import BIABar from "../ProjectsPageStudent/BIABar.jsx";
 // import Rows from "../ProjectsPageStudent/Rows.jsx";
 import Projects from "./Projects";
+import ProjectsData from "./ProjectsData";
 
 const ProjectsPageStudent = () => {
   const [isLoading, setLoading] = useState(true);
@@ -21,12 +24,26 @@ const ProjectsPageStudent = () => {
     fetchAPI();
   }, []);
 
+  // Potential solution could be mapping through an array until year level is confirmed to be correct
+  // console.log(apiData[0].Year);
+
   // function ProjectsPageStudent() {
   return (
     <div className="Layout">
       <div>
         <TopNav />
       </div>
+      <div>
+        {/*Create a div for header and sub header */}
+        <ProjectsHeader />
+      </div>
+      {/*Create a div for the sidebar */}
+      
+      {/*Create a div for BIA & Projects */}
+      {/*Create a div back to top button */}
+      {/*Change footer from absolute styling */}
+
+      {/*Old code below this line */}
       <div className="Body">
         <div className="PorjectsSideSize">
           <ProjectsSidebar />
@@ -38,10 +55,14 @@ const ProjectsPageStudent = () => {
           <BIABar />
           {/*TESTING - can replace with <Rows /> */}
           <div className="ProjectContainer">
+            {/* <ProjectsData /> = test function */}
             {apiData.map((e) => (
               <Projects
                 key={e.ProjectID}
                 image={e.MainImage}
+                // image={`data:image/jpg;base64,${btoa(
+                //   String.fromCharCode(...new Uint8Array(e.Image.data))
+                // )}`} //Usually MainImage
                 subject={e.SubjectMatter2} //e.g. Introduction
                 course={e.Course} //e.g. Beginner
                 activity={e.ActivityType} // e.g. animation
---
+++ src/img/ProjectsPageStudent/random.sql
@@ -0,0 +1,4 @@
+SELECT * FROM StudentDB.Project;
+
+ALTER TABLE StudentDB.Project
+DROP COLUMN Image;
