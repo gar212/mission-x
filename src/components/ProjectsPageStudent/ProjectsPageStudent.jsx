d9f213a32f2d3814e4c0e6133da16dc5104d2421
Sam-McCathie <sammccathie2@gmail.com>
2021-04-29 17:08:26 +1200

    Remove old code

--- src/components/ProjectsPageStudent/ProjectsPageStudent.jsx
+++ src/components/ProjectsPageStudent/ProjectsPageStudent.jsx
@@ -7,17 +7,9 @@ import ProjectBody from "./ProjectBody";
 import BackToTop from "./BackToTop";
 import Footer from "../Footer/Footer.jsx";
 
-/*Manually remove
-import ProjectsSidebar from "../ProjectsPageStudent/ProjectsPageSidebar.jsx";
-import BIABar from "../ProjectsPageStudent/BIABar.jsx";
-// import Rows from "../ProjectsPageStudent/Rows.jsx";
-import Projects from "./Projects";
-import ProjectsData from "./ProjectsData";
-*/
-
 const ProjectsPageStudent = () => {
   return (
-    <div className="Layout">
+    <div>
       <TopNav />
       <ProjectsHeader />
       <div className="Body">
